2b190bdcfaac9c5ad8e28610b0346927899b4118
Andre Luiz <97361918+AndreLuizBoanergers@users.noreply.github.com>
2023-09-09 11:59:04 -0300

    Update users.js
    criacao rota  auth

--- users.js
+++ users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router()
 const Users = require('../../models/user');
+const bcrypt = require('bcrypt');
 
 router.get("/",async (req,res)=>{
 	try{
@@ -24,6 +25,21 @@ router.post('/create', async (req,res)=>{
 		} 
 	}catch(error){
 		return res.send({error: 'Erro ao criar usuario, se o erro persistir entre em contato com o administrador!'});
+	}
+})
+
+router.post('/auth', async (req,res)=>{
+	const { email, password} = req.body;
+	try{
+		if(!email || !password) return res.send({error: 'Dados insuficientes!'});
+		const dataAuth = await Users.findOne({email}).select('+password');
+		if(!dataAuth) return res.send({error: "Usuario nao registrado!"});
+		bcrypt.compare(password, dataAuth.password, (err,same)=>{
+			if(!same) return res.send({error: 'Erro ao autenticar usuario! verifique seu usuario e senha'});
+			return res.send(dataAuth)
+		})
+	}catch(e){
+		return res.send({error: 'Erro ao autenticar usuario!'})
 	}
 })
 module.exports = router;
